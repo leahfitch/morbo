edb6dc45e098329f8320f0e3a23bd9b796caf7ae
Elisha Cook <elisha@elishacook.com>
2013-10-05 15:44:52 -0400

    Added pymongo dependency

--- setup.py
+++ setup.py
@@ -7,5 +7,8 @@ setup(
     description = 'A MongoDB object mapper for puny humans.',
     author = 'Elisha Cook',
     author_email = 'elisha@elishacook.com',
-    test_suite = "tests"
+    test_suite = "tests",
+    install_requires=[
+        'pymongo>=2.6.2'
+    ]
 )
