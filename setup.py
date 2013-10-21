48ec6c4aa44f272336f45f067d4b22f2dee4551d
Elisha Cook <elisha@elishacook.com>
2013-10-20 20:01:29 -0400

    Incremented version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'morbo',
-    version = '1.0.1',
+    version = '1.0.2',
     packages = ['morbo'],
     description = 'A MongoDB object mapper for puny humans.',
     author = 'Elisha Cook',
